bdeb7192953da8d262e8cbdf55b962f8f8636ad9
hk023455 <sudhirkumar197677@gmail.com>
2025-09-26 12:24:24 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+const WEBSITE_CONFIG = {
+    CHANNELS: {
+        MAIN: "https://t.me/+vNA6mxXv25M1MmI1",
+        SUPPORT: "https://t.me/+4Jl9Y56VnEo0OTA1", // YAHAN SUPPORT GROUP LINK DALDO
+        BACKUP: "https://t.me/+zamUHu4MKBY2YjI1"
+    },
+    
+    SUPPORT_GROUP: "https://t.me/+4Jl9Y56VnEo0OTA1", // PROOF YAHAN JAYEGA
+};
